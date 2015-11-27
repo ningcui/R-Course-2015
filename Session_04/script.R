bcf71772c5a929dede019917a7003a0fc5b02573
Andrei Beliankou <arbox@yandex.ru>
2015-11-27 17:42:01 +0100

    Upload in-class materials.

---
+++ Session_04/script.R
@@ -0,0 +1,10 @@
+v3 <- c('Ich', 'gehe', 'heute', 'nicht', 'zur', 'Uni')
+ 
+write.table(v3, 'v3.csv')
+
+       
+# write.table(v1, 'v3.csv', quote = F)
+
+# write.table(v1, 'v1.csv', quote = F, col.names = F)
+# write.table(v1, 'v1.csv', quote = F, col.names = F, row.names = F)
+# v2 <- read.table('v1.csv')
